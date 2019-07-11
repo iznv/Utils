09ca933eca73e29ea3eead80a4869e8493eeef02
Ivan Zinovyev <zinovyev-ivan@yandex.ru>
2019-07-11 19:18:47 +0300

    Add String+Constants extension

---
+++ Utils/Extensions/Swift/String/String+Constants.swift
@@ -0,0 +1,13 @@
+//
+//  String+Constants.swift
+//  Utils
+//
+//  Created by Ivan Zinovyev on 11/07/2019.
+//  Copyright © 2019 Ivan Zinovyev. All rights reserved.
+//
+
+extension String {
+    
+    static let empty = ""
+    
+}
---
+++ Utils/Utils.h
@@ -0,0 +1,19 @@
+//
+//  Utils.h
+//  Utils
+//
+//  Created by Ivan Zinovyev on 11/07/2019.
+//  Copyright © 2019 Ivan Zinovyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Utils.
+FOUNDATION_EXPORT double UtilsVersionNumber;
+
+//! Project version string for Utils.
+FOUNDATION_EXPORT const unsigned char UtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Utils/PublicHeader.h>
+
+
